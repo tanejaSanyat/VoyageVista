dbd1aebaab76e229e1a589c79ff5be9748a4d52d
tanejaSanyat <sanyat.taneja07@gmail.com>
2024-05-03 16:31:48 +0530

    finalll

--- src/pages/Dashboard/ManageBooking.jsx
+++ src/pages/Dashboard/ManageBooking.jsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
-import { useAuth } from "../../components/Context/AuthContext";
 import Sidebar from "../../components/Dashboard/Sidebar";
 import Message from "../../components/Message/Message";
 const ManageBooking = () => {
